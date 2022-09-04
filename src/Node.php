805fbee5c90ed42220b92ca210ccea03aaa6c56b
VennV <111500380+VennDev@users.noreply.github.com>
2022-09-04 16:04:34 +0700

    Update Node.php

--- src/Node.php
+++ src/Node.php
@@ -11,4 +11,20 @@ class Node{
         $this->next = $next;
         $this->prev = $prev;
     }
+    
+    public function __toString() : string{
+        return (string) $this->item;
+    }
+
+    public function __clone() : void{
+        $this->set($this->prev, $this->item, $this->next);
+    }
+
+    public function __debugInfo() : array{
+        return [
+            "item" => $this->item,
+            "next" => $this->next,
+            "prev" => $this->prev
+        ];
+    }
 }
